baafd027936ba95de50a32ce4edaadcbd4f5f948
duqz2001 <zachary.duquette@usherbrooke.ca>
2015-09-18 17:48:07 -0400

    Fixed sdl include/lib bullshit
    Former-commit-id: 115816892bc419f9573d3c32b9f00a9023dc4f9a

--- GreenShells/GreenShells/main.cpp
+++ GreenShells/GreenShells/main.cpp
@@ -1,41 +1,11 @@
-//#include <iostream>
-//#include <SDL.h>
-//
-//int main(int, char**) {
-//	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-//		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-//		return 1;
-//	}
-//	SDL_Quit();
-//	return 0;
-//}
-
-#include <boost/lambda/lambda.hpp>
 #include <iostream>
-#include <iterator>
-#include <algorithm>
-//#include "GameSession.h"
-//
-//int main()
-//{
-//	using namespace boost::lambda;
-//	typedef std::istream_iterator<int> in;
-//
-//	std::for_each(
-//		in(std::cin), in(), std::cout << (_1 * 3) << " ");
-//
-//
-//	GameSession* session = new GameSession;
-//}
+#include <SDL.h>
 
-#include <boost/array.hpp>
-#include <boost/asio.hpp>
-
-int main()
-{
-	using namespace boost::lambda;
-	typedef std::istream_iterator<int> in;
-
-	std::for_each(
-		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+int main(int, char**) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Quit();
+	return 0;
 }
