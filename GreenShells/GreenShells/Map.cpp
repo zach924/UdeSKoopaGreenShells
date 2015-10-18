693112a42bd2d50f3dacfee0e1553c0a09f10b60
clom1806 <maxime.cloutier3@usherbrooke.ca>
2015-10-18 13:07:55 -0400

    Fixed the map according to julien commit
    Former-commit-id: 2fd4ae90dd1ff13d1cf87f1452091fd10833771a

--- GreenShells/GreenShells/Map.cpp
+++ GreenShells/GreenShells/Map.cpp
@@ -76,16 +76,16 @@ void Map::GenerateTiles()
             switch (tileType)
             {
             case '0':
-				m_tiles[i][j] = new TileGround(Position(i, j));
+				m_tiles[i][j] = new TileGround(Position(j, i));
                 break;
 
             case '1':
-				m_tiles[i][j] = new TileMountain(Position(i, j));
+				m_tiles[i][j] = new TileMountain(Position(j, i));
                 break;
 
             case '2':
             default:
-				m_tiles[i][j] = new TileWater(Position(i, j));
+				m_tiles[i][j] = new TileWater(Position(j, i));
                 break;
             }
         }
@@ -106,7 +106,7 @@ std::vector<TileBase*> Map::GetArea(Position position, int distance)
     {
         for (int j = minRow; j <= maxRow; ++j)
         {
-            area.push_back(GetTile(Position(i, j)));
+            area.push_back(GetTile(Position(j, i)));
         }
     }
     return area;
