f1573ac06d1d9491e6e711a590bd4986ab9dfab5
duqz2001 <zachary.duquette@usherbrooke.ca>
2015-11-09 11:07:13 -0500

    Code review changes
    Former-commit-id: 67b88029fb1fdc09ee7ac179fb7e3ca73b121148

--- GreenShells/GreenShells/Position.h
+++ GreenShells/GreenShells/Position.h
@@ -20,6 +20,6 @@ public:
 
     inline bool operator==(const Position& other) const { return Row == other.Row && Column == other.Column; }
     inline bool operator!=(const Position& other) const { return !(*this == other); }
-    //For usage in a map
+    //For usage in a std::map
     inline bool operator< (const Position& other) const { return Row < other.Row && Column < other.Column; }
 };
