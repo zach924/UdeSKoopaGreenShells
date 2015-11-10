bb02129d15adbf3a0e017af88cb06cf9ba201657
duqz2001 <zachary.duquette@usherbrooke.ca>
2015-11-10 11:10:05 -0500

    Added general button that takes a text and a lambda in parameter
    Former-commit-id: c1af9d4955fadf3e37d45fe63db3b5b067d7b360

---
+++ GreenShells/GreenShells/ButtonText.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+#include "ButtonText.h"
+
+ButtonText::ButtonText(int posX, int posY, int width, int height, std::string text, std::function<void()> lambda, ButtonState state)
+    :Button(posX, posY, width, height, state),
+    m_text(text),
+    m_function(lambda)
+{
+
+}
+
+ButtonText::~ButtonText()
+{
+}
+
+void ButtonText::DoAction()
+{
+    m_buttonState = ButtonState::Pressed;
+    m_function();
+    m_buttonState = ButtonState::Unpressed;
+    
+}
+
+void ButtonText::LoadTextTexture(SDL_Renderer * rend)
+{
+    try
+    {
+        TTF_Font* font = TTF_OpenFont("..\\Fonts\\roboto\\Roboto-BlackItalic.ttf", 30);
+        m_textTexture.CreateFromText(m_text, font, rend);
+    }
+    catch (std::exception e)
+    {
+        std::string msg{ e.what() };
+        std::cout << msg << std::endl;
+    }
+}
